2bc91b91bd52e57b1dbbe3c3bab8966a828b33ed
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2019-05-26 14:19:08 +0430

    "extract-lemmatized-nonstop-words": "^1.0.16"

--- test.js
+++ test.js
@@ -11,17 +11,17 @@ test('grade the sample text', t => {
         the long braids of a river gleaming at the bottom of a canyon.
         Above the wing the sky had deepened to a blue so pure he knew it would bring tears to his eyes if he looked long enough.
     `);
-    t.is(output.words.length, 50);
+    t.is(output.words.length, 51);
     t.is(output.words[0][0], 'with');
     t.is(output.words[0][1], 2);
     t.is(output.meta.words, 59);
     t.is(output.meta.grade, 'C2');
-    t.is(output.meta.mean, 1961.406779661017);
-    t.is(output.meta.max, 9858);
-    t.is(output.meta.levels.A1, 35.18518518518518);
-    t.is(output.meta.levels.A2, 42.592592592592595);
-    t.is(output.meta.levels.B1, 53.7037037037037);
-    t.is(output.meta.levels.B2, 70.37037037037037);
-    t.is(output.meta.levels.C1, 85.18518518518519);
+    t.is(output.meta.mean, 1849.5762711864406);
+    t.is(output.meta.max, 8634);
+    t.is(output.meta.levels.A1, 34.54545454545455);
+    t.is(output.meta.levels.A2, 43.63636363636363);
+    t.is(output.meta.levels.B1, 54.54545454545455);
+    t.is(output.meta.levels.B2, 72.72727272727273);
+    t.is(output.meta.levels.C1, 87.27272727272727);
     t.is(output.meta.levels.C2, 100);
 });
